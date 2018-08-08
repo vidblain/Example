5aebe6516b0eb493fae68eb80f45a85a6ee5693f
Jorge Atempa <jorge.atempa@tagwizz.com.mx>
2018-08-08 14:11:09 -0700

    Interpolar diferentes tipos de datos

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
-print("Introduce tú nombre:")
-name = input()
-print(f"Bienvenido {name}")
+i = 28
+print(f"i is {i}")
+
+f = 2.8
+print(f"f is {f}")
+
+b = True
+print(f"b is {b}")
+
+n = None
+print(f"n is {n}")
