2b630a949e953a2925883e899cd6663c591a79c4
Jorge Atempa <jorge.atempa@tagwizz.com.mx>
2018-08-08 14:12:01 -0700

    Evaluar estructuras condicionales

--- demo.py
+++ demo.py
@@ -1,11 +1,8 @@
-i = 28
-print(f"i is {i}")
+x = int(input())
 
-f = 2.8
-print(f"f is {f}")
-
-b = True
-print(f"b is {b}")
-
-n = None
-print(f"n is {n}")
+if x > 0:
+    print("x is positive")
+elif x < 0:
+    print("x is negative")
+else:
+    print("x is 0")
